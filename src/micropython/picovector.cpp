d5d719fba53b009f88cd2d9f511c46ed271afd61
Phil Howard <github@gadgetoid.com>
2025-10-14 22:14:18 +0100

    Make input a nested module, finalize images in all cases.

--- src/micropython/picovector.cpp
+++ src/micropython/picovector.cpp
@@ -22,24 +22,20 @@ extern "C" {
 
   #include "py/runtime.h"
 
-  extern const mp_obj_type_t type_PicoVector;
-
   typedef struct _modpicovector_obj_t {
     bool initialised;
     image_obj_t *image;
-    input_obj_t *input;
   } modpicovector_obj_t;
   
 
   int screen_width = 160;
   int screen_height = 120;
   uint32_t framebuffer[160 * 120];
-  image_t screen(framebuffer, screen_width, screen_height);
 
   mp_obj_t modpicovector___init__(void) {
     modpicovector_obj_t *state = (modpicovector_obj_t *)MP_STATE_VM(modpicovector_state);
 
-    if(state && state->initialised && mp_obj_is_type(state->image, &type_Image) && mp_obj_is_type(state->input, &type_Input)) return mp_const_none;
+    if(state && state->initialised && mp_obj_is_type(state->image, &type_Image)) return mp_const_none;
 
     state = (modpicovector_obj_t *)m_malloc(sizeof(modpicovector_obj_t));
 
@@ -47,9 +43,8 @@ extern "C" {
   
     // Don't use mp_obj_malloc_with_finalizer here, since the __del__
     // method will try to delete our `screen` and explode.
-    state->image =  mp_obj_malloc(image_obj_t, &type_Image);
-    state->image->image = &screen;
-    state->input = mp_obj_malloc(input_obj_t, &type_Input);
+    state->image =  mp_obj_malloc_with_finaliser(image_obj_t, &type_Image);
+    state->image->image = new(m_malloc(sizeof(image_t))) image_t(framebuffer, screen_width, screen_height);
 
   #ifdef PICO
     // we need a way to set this up, but if the user wants to use the
@@ -66,6 +61,11 @@ extern "C" {
 
     MP_STATE_VM(modpicovector_state) = state;
 
+    // Load and modify our module to add the "screen" attribute
+    // this doesn't work - sadly - because imports happen before __init__
+    //mp_obj_t module = mp_obj_new_module(MP_QSTR_picovector);
+    //mp_store_attr(module, MP_QSTR_screen, MP_OBJ_FROM_PTR(state->image));
+
     return mp_const_none;
   }
   MP_REGISTER_ROOT_POINTER(void* modpicovector_state);
@@ -76,9 +76,6 @@ extern "C" {
     if (dest[0] == MP_OBJ_NULL) {
       if (attr == MP_QSTR_screen) {
         dest[0] = MP_OBJ_FROM_PTR(state->image);
-      }
-      if (attr == MP_QSTR_io) {
-        dest[0] = MP_OBJ_FROM_PTR(state->input);
       }
     }
   }
--- src/micropython/picovector_bindings.c
+++ src/micropython/picovector_bindings.c
@@ -1,9 +1,6 @@
 
 #include "py/runtime.h"
 
-extern void modpicovector_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest);
-extern mp_obj_t modpicovector___init__(void);
-
 extern const mp_obj_type_t type_Shapes;
 extern const mp_obj_type_t type_Brushes;
 extern const mp_obj_type_t type_Image;
@@ -11,6 +8,41 @@ extern const mp_obj_type_t type_Font;
 extern const mp_obj_type_t type_PixelFont;
 extern const mp_obj_type_t type_Matrix;
 
+// modinput
+#define BUTTON_HOME 0b100000
+#define BUTTON_A    0b010000
+#define BUTTON_B    0b001000
+#define BUTTON_C    0b000100
+#define BUTTON_UP   0b000010
+#define BUTTON_DOWN 0b000001
+
+extern void modinput_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest);
+extern mp_obj_t modinput_poll();
+static MP_DEFINE_CONST_FUN_OBJ_0(modinput_poll_obj, modinput_poll);
+
+const mp_rom_map_elem_t modinput_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_poll), MP_ROM_PTR(&modinput_poll_obj) },
+    { MP_ROM_QSTR(MP_QSTR_BUTTON_HOME), MP_ROM_INT(BUTTON_HOME) },
+    { MP_ROM_QSTR(MP_QSTR_BUTTON_A),    MP_ROM_INT(BUTTON_A) },
+    { MP_ROM_QSTR(MP_QSTR_BUTTON_B),    MP_ROM_INT(BUTTON_B) },
+    { MP_ROM_QSTR(MP_QSTR_BUTTON_C),    MP_ROM_INT(BUTTON_C) },
+    { MP_ROM_QSTR(MP_QSTR_BUTTON_UP),   MP_ROM_INT(BUTTON_UP) },
+    { MP_ROM_QSTR(MP_QSTR_BUTTON_DOWN), MP_ROM_INT(BUTTON_DOWN) },
+};
+
+static MP_DEFINE_CONST_DICT(modinput_globals, modinput_globals_table);
+const mp_obj_module_t modinput = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t *)&modinput_globals,
+};
+
+MP_REGISTER_MODULE(MP_QSTR__io, modinput);
+MP_REGISTER_MODULE_DELEGATION(modinput, modinput_attr);
+
+
+// modpicovector
+extern void modpicovector_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest);
+extern mp_obj_t modpicovector___init__(void);
 static MP_DEFINE_CONST_FUN_OBJ_0(modpicovector___init___obj, modpicovector___init__);
 
 static const mp_rom_map_elem_t modpicovector_globals_table[] = {
@@ -22,8 +54,10 @@ static const mp_rom_map_elem_t modpicovector_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_Font),  MP_ROM_PTR(&type_Font) },
     { MP_ROM_QSTR(MP_QSTR_PixelFont),  MP_ROM_PTR(&type_PixelFont) },
     { MP_ROM_QSTR(MP_QSTR_Matrix),  MP_ROM_PTR(&type_Matrix) },
+    { MP_ROM_QSTR(MP_QSTR_io),  MP_ROM_PTR(&modinput) },
 };
 static MP_DEFINE_CONST_DICT(modpicovector_globals, modpicovector_globals_table);
+
 
 // Define module object.
 const mp_obj_module_t modpicovector = {
