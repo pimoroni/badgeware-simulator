06d4852630bfeb023fec4adf5ccb8c00610aa40d
Phil Howard <github@gadgetoid.com>
2025-10-21 16:11:56 +0100

    Clean up after menu in main.py

--- root/system/main.py
+++ root/system/main.py
@@ -2,6 +2,7 @@
 import gc
 import sys
 import os
+import micropython
 
 SKIP_CINEMATIC = True
 APP_STARTUP = "/system/apps/startup"
@@ -11,21 +12,27 @@ app = None
 def launch(file):
     global app
     if app is not None:
+        print(f"main.py: Tearing down {app.__name__} {sys.modules}")
         getattr(app, "on_exit", lambda: None)()
+        #old_name = app.__name__
         del app
         # Strip out any paths relating to "/system/apps"
         while sys.path[0].startswith("/system/apps"):
             sys.path.pop(0)
         # Hack: If we don't "unimport" modules then an app will used a cached version
         # eg: Mona Pet's "import ui" will get the menu's "ui"
-        for module in ["ui", "icon"]:
-            if module in sys.modules:
-                del sys.modules[module]
-    gc.collect()
+        #for module in ["ui", "icon", old_name, "pcf85063a", "powman", "st7789"]:
+        #    if module in sys.modules:
+        #        del sys.modules[module]
+        for module in sys.modules.keys():
+            del sys.modules[module]
     # Fix up the paths so the app can find its modules and assets
     sys.path.insert(0, file)
     os.chdir(file)
+    gc.collect()
+    print(micropython.mem_info())
     app = __import__(file)
+    print(f"main.py: Launched {app.__name__}")
     getattr(app, "init", lambda: None)()
 
 
