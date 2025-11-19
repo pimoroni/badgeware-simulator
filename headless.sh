bc7e55acdc12cf31d60fd7cb9f4edbd3248dea1a
Phil Howard <github@gadgetoid.com>
2025-11-19 15:20:42 +0000

    Headless: More debug output.

--- headless.sh
+++ headless.sh
@@ -5,18 +5,20 @@ PYTHON_SCRIPT="$1"
 TIME_SECONDS="$2"
 OUTPUT_FILE="$3"
 
-printf "Running in $BADGEWARE_SIMULATOR\n"
-printf "Python script: $PYTHON_SCRIPT\n"
+printf "Headless: Running script $PYTHON_SCRIPT for $TIME_SECONDS sec.\n"
+printf "Running in $BADGEWARE_SIMULATOR, output $OUTPUT_FILE\n"
 
 cp "$PYTHON_SCRIPT" "$BADGEWARE_SIMULATOR/root/record_app.py"
 cd "$BADGEWARE_SIMULATOR"
 
 ./build/micropython record_app $TIME_SECONDS
 
-if ["$TIME_SECONDS" -eq "0"]; then
+if [[ "$TIME_SECONDS" -eq "0" ]]; then
+    printf "Copying $BADGEWARE_SIMULATOR/screenshots/record_app-000001.png to $OUTPUT_FILE\n"
     cp "$BADGEWARE_SIMULATOR/screenshots/record_app-000001.png" "$OUTPUT_FILE"
 else
-    ffmpeg -pattern_type glob -r 60 -i 'screenshots/record_app-*.png' -c:v libx264 -preset ultrafast -crf 18 -vf scale=640:480:flags=neighbor -fps_mode passthrough "$OUTPUT_FILE"
+    printf "Generating video from $BADGEWARE_SIMULATOR/screenshots/record_app-*.png and saving to $OUTPUT_FILE\n"
+    ffmpeg -y -pattern_type glob -r 60 -i 'screenshots/record_app-*.png' -c:v libx264 -preset ultrafast -crf 18 -vf scale=640:480:flags=neighbor -fps_mode passthrough "$OUTPUT_FILE"
 fi
 
-rm -r "$BADGEWARE_SIMULATOR/screenshots/record_app-*.png"
+rm -r $BADGEWARE_SIMULATOR/screenshots/record_app-*.png
