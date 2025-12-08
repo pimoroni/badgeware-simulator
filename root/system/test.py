bef4fa7ce35952e95a2ac953df3483c23d532465
Phil Howard <github@gadgetoid.com>
2025-12-08 15:53:37 +0000

    Add brush test.

--- root/system/test.py
+++ root/system/test.py
@@ -1,46 +1,37 @@
-#mode(LORES | PALETTED)
+mode(HIRES)
 
 screen.font = PixelFont.load("/system/assets/fonts/sins.ppf")
 
-# red = color(255, 0, 0)
-# red = from_okclh(0, 100, 100)
-# green = color(0, 255, 0)
-# red.alpha =
-# red.blend(blue, 50)
-
-
+import math
 
 def update():
+  pattern = brush.pattern(color.rgb(255, 0, 0, 255), color.rgb(0, 0, 255, 100), (
+    0b00000000,
+    0b01111110,
+    0b01000010,
+    0b01011010,
+    0b01011010,
+    0b01000010,
+    0b01111110,
+    0b00000000))
+  pattern = brush.pattern(color.rgb(255, 0, 0, 255), color.rgb(0, 0, 255, 255), 11)
+  pen(pattern)
+  screen.rectangle(0, 0, screen.width, screen.height)
 
-  red = color.rgb(255, 0, 0, 100)
+  pattern = brush.pattern(color.rgb(255, 0, 255, 255), color.rgb(0, 0, 0, 0), (
+    0b00000000,
+    0b01111110,
+    0b01000010,
+    0b01011010,
+    0b01011010,
+    0b01000010,
+    0b01111110,
+    0b00000000))
+  pattern = brush.pattern(color.rgb(255, 0, 255, 255), color.rgb(0, 0, 0, 0), 11)
 
-  for y in range(120):
-    for x in range(160):
-      if y < 60:
-        col = color.hsv(int(360 * (x / 160)), 255 - y, 155)
-      else:
-        col = color.oklch(155 , 155, int(360 * (x / 160)))
-
-      if x % 2 == 0:
-        col.blend(red)
-      screen.brush = brush.color(col.r, col.g, col.b)
-
-      screen.rectangle(x, y, 1, 1)
-
-
-  # color(red)
-  # blur(10)
-
-  # xor_brush = xor(color(0, 255, 0))
-
-  # brush(xor_brush)
-
-
-
-  # clear()
-
-  # brush(solid(color(0, 0, 255)))
-
-  # text("Hello World", (io.ticks/20)%100, 0)
+  pen(pattern)
+  
+  screen.antialias = Image.X4
+  shapes.circle(160 + math.sin(io.ticks / 500) * 30, 120 + math.sin(io.ticks / 1000) * 30, 80).draw()
 
   return True
